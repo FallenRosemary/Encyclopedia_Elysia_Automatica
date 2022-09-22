845032ffbd42b09945355d25479abc9714aafee8
Elysian Rosemary <wmaakor3@gmail.com>
2022-09-22 20:19:59 +0200

    fixed wrong path location for raw ore blocks

--- kubejs/startup_scripts/Materials/MaterialsBlocks - Registry.js
+++ kubejs/startup_scripts/Materials/MaterialsBlocks - Registry.js
@@ -3,7 +3,7 @@ onEvent('block.registry',(event) => {
     // Metals
     global.metals.forEach((metal) => {
         event.create(`block_${metal.name}`).displayName(`Block of ${metal.display}`).textureAll(`kubejs:unification/${metal.name}/block`);
-        event.create(`block_raw_${metal.name}`).displayName(`Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_living`);
+        event.create(`block_raw_${metal.name}`).displayName(`Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_raw`);
         event.create(`block_raw_compressed_${metal.name}`).displayName(`Compressed Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_compressed`);
         event.create(`block_gilded_${metal.name}`).displayName(`Gilded Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_gilded`);
         event.create(`block_living_${metal.name}`).displayName(`Living Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_living`);
