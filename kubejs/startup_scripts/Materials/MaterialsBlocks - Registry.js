2d67de10779b91ec43a40d3f8745a394a2724e3f
Leo Puvilland <leo@craftcat.dev>
2022-09-23 09:20:10 -0700

    Add ores/rocks to registry

--- kubejs/startup_scripts/Materials/MaterialsBlocks - Registry.js
+++ kubejs/startup_scripts/Materials/MaterialsBlocks - Registry.js
@@ -7,5 +7,9 @@ onEvent('block.registry',(event) => {
         event.create(`block_raw_compressed_${metal.name}`).displayName(`Compressed Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_compressed`);
         event.create(`block_gilded_${metal.name}`).displayName(`Gilded Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_gilded`);
         event.create(`block_living_${metal.name}`).displayName(`Living Block of Raw ${metal.veinDisplay}`).textureAll(`kubejs:unification/${metal.name}/block_living`);
+        // Ores
+        global.rocks.forEach((rock) => {
+            event.create(`${rock}_${metal}_ore`).displayName('${rock} ${metal} Ore').textureAll(`kubejs:blocks/${rock}_${metal}`)
+         })
     });
 })
