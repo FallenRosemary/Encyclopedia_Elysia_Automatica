c10c315b29226c4d6ebf23aa49be38ad1d89fceb
Elysian Rosemary <wmaakor3@gmail.com>
2022-09-26 21:34:06 +0200

    updated to 1.19.2 (entirely possible i messed this up somehow)

--- kubejs/startup_scripts/Materials/MaterialsGases- Registry.js
+++
@@ -1,8 +0,0 @@
-onEvent('mekanism.slurry.registry',(event) => {
-
-    global.metals.forEach((metal) => {
-        event.create(`clean_slurry_${metal.name}`).displayName(`Clean ${metal.display} Slurry`).texture(`kubejs:block/clean_slurry_${metal.name}`);
-        event.create(`dirty_slurry_${metal.name}`).displayName(`Dirty ${metal.display} Slurry`).texture(`kubejs:block/dirty_slurry_${metal.name}`);
-    });
-})
-
